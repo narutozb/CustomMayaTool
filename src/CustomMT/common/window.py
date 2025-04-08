aa62019c76b83882336ac9c0d02b3a78721a5eab
qiaoyuanzhen <narutozb@hotmail.com>
2025-04-09 00:58:23 +0900

    添加实验性基础功能

---
+++ src/CustomMT/common/window.py
@@ -0,0 +1,50 @@
+from maya import cmds, mel
+
+
+def get_main_window():
+    """
+    获取 Maya 主窗口的名称
+    :return: 主窗口名称
+    """
+
+    return mel.eval('$tmpVar=$gMainWindow')
+
+
+def close_windows(name_list: list[str] = None):
+    """
+    关闭指定名称的窗口
+    :param name_list: 窗口名称列表
+    """
+    result = []
+    for name in name_list:
+        if cmds.window(name, exists=True):
+            if name != get_main_window():
+                try:
+                    cmds.deleteUI(name)
+                    result.append(name)
+                except Exception as e:
+                    # 忽略可能出现的错误，例如窗口已被删除
+                    raise e
+    return result
+
+
+def close_all_windows(exclude_windows: list[str] = None):
+    """
+    关闭所有窗口，排除指定的窗口
+    """
+    result = []
+    # 获取当前打开的所有窗口
+    open_windows = cmds.lsUI(windows=True)
+    for window in open_windows:
+        # 如果窗口在排除列表中，则跳过
+        if exclude_windows and window in exclude_windows:
+            continue
+        # 排除主 Maya 窗口
+        if window != get_main_window() and cmds.window(window, exists=True):
+            try:
+                cmds.deleteUI(window, window=True)
+                result.append(window)
+            except Exception as e:
+                # 忽略可能出现的错误，例如窗口已被删除
+                raise e
+    return result
